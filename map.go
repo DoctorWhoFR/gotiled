b019c9b40fb0d92ec0be5d5d814d3bb5f95d28dc
4Azgin <doctorwhofr@protonmail.com>
2023-07-01 13:56:05 +0200

    update generation, and add inventory system

--- map.go
+++ map.go
@@ -16,11 +16,26 @@ import (
 	"image/png"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 
 	"github.com/fogleman/gg"
 	"github.com/google/uuid"
 )
+
+type TextRGBA struct {
+	R int
+	G int
+	B int
+}
+
+type Text struct {
+	Message string
+	X       int
+	Y       int
+	Size    int
+	RGB     TextRGBA
+}
 
 /*
 Map
@@ -36,7 +51,8 @@ A map is based on
 type Map struct {
 	TileMap                   // Representation of an TileMap currently the scene rendered to the user
 	Sprites map[string]Sprite // Sprites representations representation of an image in the 2D scene
-	Message string            // For in-build image notification system
+	Texts   []Text
+	Message string // For in-build image notification system
 	Debug   bool
 }
 
@@ -177,75 +193,16 @@ func (m *Map) SendNotification(message string) {
 }
 
 /*
-RenderScene
-
-Main function used to get the final Image png representation of a user Map.
-
-When you are using RenderScene every Sprite's that is present on the Map will be generated.
-
-Linked to their equivalent Sprite.Level if having one.
-
-Or displayed as their base image Sprite.Image if having no level.
-
-You will get a string path, where the generated file belong.
-
-/!\ *The file will NOT BE automatically delete, so you need to handle this part.* /!\
-*/
-func (m *Map) RenderScene() string {
-	reader := bytes.NewReader(m.BaseImage)
-	base, _ := png.Decode(reader)
-	rgba := image.NewRGBA(base.Bounds())
-
-	m.RGBA = rgba
-
-	dc := gg.NewContextForImage(rgba)
-	dc.SetRGB(255, 255, 255)
-	dc.Clear()
-	dc.DrawImage(base, 0, 0)
-
-	baseX := base.Bounds().Dx()
-	baseY := base.Bounds().Dy()
-
-	maxX := baseX / m.GridSize
-	maxY := baseY / m.GridSize
-
-	m.renderSprites(dc)
-
-	m.renderMessage(dc)
-
-	m.renderDebug(maxY, dc, maxX)
-
-	largerDC := m.scaleUP(baseX, baseY, dc)
-
-	// Open the PNG image file
-	currentDir, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Failed to get current directory:", err)
-	}
-
-	_uuid, _ := uuid.NewUUID()
-	_filename := fmt.Sprintf(currentDir+"\\tmp_%s.png", _uuid.String())
-
-	err = largerDC.SavePNG(_filename)
-
-	if err != nil {
-		return ""
-	}
-
-	return _filename
-}
-
-/*
 scaleUP
 
 Simple DiscordInternal function used to scale UP the final scene by 2.
 */
 func (m *Map) scaleUP(baseX int, baseY int, dc *gg.Context) *gg.Context {
 	// Create a larger context for scaling up
-	largerDC := gg.NewContext(int(float64(baseX)*2), int(float64(baseY)*2))
+	largerDC := gg.NewContext(int(float64(baseX)*3), int(float64(baseY)*3))
 
 	// Scale the drawing to the larger context
-	largerDC.Scale(2, 2)
+	largerDC.Scale(3, 3)
 	largerDC.DrawImage(dc.Image(), 0, 0)
 	return largerDC
 }
@@ -259,12 +216,14 @@ func (m *Map) renderSprites(dc *gg.Context) {
 
 	// create a zindex map of sprite
 	var ZSpritesMap = make(map[int][]Sprite)
+	var ZSPritesMapIndex = make([]int, 0)
 
 	for _, sprite := range m.Sprites {
 		zsprite, finded := ZSpritesMap[sprite.ZIndex]
 
 		if !finded {
 			zsprite = []Sprite{}
+			ZSPritesMapIndex = append(ZSPritesMapIndex, sprite.ZIndex)
 		}
 
 		zsprite = append(zsprite, sprite)
@@ -272,8 +231,10 @@ func (m *Map) renderSprites(dc *gg.Context) {
 		ZSpritesMap[sprite.ZIndex] = zsprite
 	}
 
-	for i := 0; i < len(ZSpritesMap); i++ {
-		zsprites := ZSpritesMap[i+1]
+	sort.Ints(ZSPritesMapIndex)
+
+	for _, index := range ZSPritesMapIndex {
+		zsprites := ZSpritesMap[index]
 		for _, sprite := range zsprites {
 			if sprite.Level > 1 {
 				_lvl := sprite.Levels[sprite.Level]
@@ -284,16 +245,31 @@ func (m *Map) renderSprites(dc *gg.Context) {
 				m.DrawImage(sprite, dc)
 			}
 		}
+
 	}
 
 }
 
+func (m *Map) drawText(dc *gg.Context, text string, x, y, size int, rgb TextRGBA) {
+	xpos, ypos := m.GridToPixel(x, y)
+	// Open the PNG image file
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Failed to get current directory:", err)
+	}
+	if err := dc.LoadFontFace(currentDir+"\\assets\\fonts\\Roboto-Light.ttf", float64(size)); err != nil {
+		panic(err)
+	}
+	dc.SetRGB255(rgb.R, rgb.G, rgb.B)
+	dc.DrawString(text, float64(xpos), float64(ypos))
+}
+
 /*
-renderMessage
+renderNotification
 
 Simple DiscordInternal function to render in-build notification message
 */
-func (m *Map) renderMessage(dc *gg.Context) {
+func (m *Map) renderNotification(dc *gg.Context) {
 	if m.Message != "" {
 		base3, _, err := LoadImage("\\assets\\gui.png")
 
@@ -342,22 +318,86 @@ Simple DiscordInternal function to the final scene with a debugging grids.
 func (m *Map) renderDebug(maxY int, dc *gg.Context, maxX int) {
 	if m.Debug {
 		for y := 0; y < maxY; y++ {
-			fmt.Println(10 + float64(y*16))
+			fmt.Println(10 + float64(y*m.GridSize))
 
 			dc.SetRGB(255, 0, 21)
-			dc.DrawString(strconv.Itoa(y), 3, 10+float64(y*16))
+			dc.DrawString(strconv.Itoa(y), 3, 10+float64(y*m.GridSize))
 
 			for x := 0; x < maxX; x++ {
 				if y == 0 {
 					dc.SetRGB(255, 0, 21)
-					dc.DrawString(strconv.Itoa(x), 3+float64(x*16)-float64(y), 10)
+					dc.DrawString(strconv.Itoa(x), 3+float64(x*m.GridSize)-float64(y), 10)
 				}
 				dc.Push()
-				dc.DrawRectangle(float64(x*16), float64(y*16), 15, 15)
+				dc.DrawRectangle(float64(x*m.GridSize), float64(y*m.GridSize), 15, 15)
 				dc.SetLineCap(2)
 				dc.SetRGBA(0, 0, 0, 0.5)
 				dc.Fill()
 			}
 		}
 	}
+}
+
+/*
+RenderScene
+
+Main function used to get the final Image png representation of a user Map.
+
+When you are using RenderScene every Sprite's that is present on the Map will be generated.
+
+Linked to their equivalent Sprite.Level if having one.
+
+Or displayed as their base image Sprite.Image if having no level.
+
+You will get a string path, where the generated file belong.
+
+/!\ *The file will NOT BE automatically delete, so you need to handle this part.* /!\
+*/
+func (m *Map) RenderScene() string {
+	reader := bytes.NewReader(m.BaseImage)
+	base, _ := png.Decode(reader)
+	rgba := image.NewRGBA(base.Bounds())
+
+	m.RGBA = rgba
+
+	dc := gg.NewContextForImage(rgba)
+	dc.SetRGB(255, 255, 255)
+	dc.Clear()
+	dc.DrawImage(base, 0, 0)
+
+	baseX := base.Bounds().Dx()
+	baseY := base.Bounds().Dy()
+
+	maxX := baseX / m.GridSize
+	maxY := baseY / m.GridSize
+
+	m.renderSprites(dc)
+
+	m.renderNotification(dc)
+
+	m.renderDebug(maxY, dc, maxX)
+
+	for _, text := range m.Texts {
+		m.drawText(dc, text.Message, text.X, text.Y, text.Size, text.RGB)
+	}
+	dc.SetRGB(255, 255, 255)
+
+	largerDC := m.scaleUP(baseX, baseY, dc)
+
+	// Open the PNG image file
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Failed to get current directory:", err)
+	}
+
+	_uuid, _ := uuid.NewUUID()
+	_filename := fmt.Sprintf(currentDir+"\\assets\\tmp\\tmp_%s.png", _uuid.String())
+
+	err = largerDC.SavePNG(_filename)
+
+	if err != nil {
+		return ""
+	}
+
+	return _filename
 }
